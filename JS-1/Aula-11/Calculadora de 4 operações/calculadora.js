3c55c23552311d6df591b564c0f72cbbe889011d
GabrielCStabile <devstabile@gmail.com>
2023-05-22 22:02:48 -0300

    Adição das aulas 10 e 11

---
+++ JS-1/Aula-11/Calculadora de 4 operações/calculadora.js
@@ -0,0 +1,29 @@
+/**
+ * Escreva um programa em js que permita inserir dois valores numéricos e então calcule o
+ * resultado das quatro operações básicas (soma, subtração, multiplicação e divisão.)
+ *
+ * Após calcular os resultados o programa deve exibi-los na tela.
+ */
+
+const num1 = prompt("Informe o primeiro número:");
+const num2 = prompt("Informe o segundo número:");
+
+const x = parseFloat(num1);
+const y = parseFloat(num2);
+
+const soma = x + y;
+const subtracao = x - y;
+const multiplicacao = x * y;
+const divisao = x / y;
+
+alert(
+  "Resultados\n" +
+    "Soma: " +
+    soma +
+    "\nSubtração: " +
+    subtracao +
+    "\nMultiplicação: " +
+    multiplicacao +
+    "\nDivisão: " +
+    divisao
+);
