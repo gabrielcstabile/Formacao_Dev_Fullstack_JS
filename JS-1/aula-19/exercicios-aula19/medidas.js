78719e6d09150f2bf4546c6283e809cd4e0b04e1
GabrielCStabile <devstabile@gmail.com>
2023-05-24 21:40:53 -0300

    Switch Case

---
+++ JS-1/aula-19/exercicios-aula19/medidas.js
@@ -0,0 +1,13 @@
+/**
+Escreva um programa que funcione como um conversor de medidas. O programa deverá pedir por um valor em metros
+e então dar a opção de escolher para qual unidade de medida esse valor deve ser convertido. As opções são:
+1- milímetro (mm)
+2- centímetro (cm)
+3- decímetro (dm)
+4- decâmetro (dam)
+5- hectômetro (hm)
+6- quilômetro (km)
+
+O programa deve estão converter a medida de acordo com a opção escolhida e exibir o resultado.
+O programa deve exibir uma mensagem de "Opção inválida" caso o usuário insira uma opção diferente das disponíveis.
+ */
---
+++ JS-1/aula-19/switch.js
@@ -0,0 +1,18 @@
+const resultado = prompt("Escolha uma alternativa:\n a)\nb)\nc)");
+
+switch (resultado) {
+  case "a":
+    alert("a");
+    break;
+
+  case "b":
+    alert("b");
+    break;
+
+  case "c":
+    alert("c");
+    break;
+
+  default:
+    alert("Opção inválida");
+}
